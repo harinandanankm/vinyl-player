0361a085b09243b3a8d2cdc918c429dd317ccb13
harinandanankm <harinandanankm@gmail.com>
2026-03-16 18:12:02 +0530

    fix vinyl size to fill platter and center align

--- src/components/RecordPlayer.tsx
+++ src/components/RecordPlayer.tsx
@@ -76,8 +76,10 @@ export function RecordPlayer({
               onTouchStart={onMouseDown}
               className={slideClass}
               style={{
-                width: "100%", height: "100%",
-                borderRadius: "50%", position: "relative",
+                width: "300px", height: "300px",
+                borderRadius: "50%", position: "absolute",
+                top: "50%", left: "50%",
+                marginTop: "-150px", marginLeft: "-150px",
                 cursor: isDragging ? "grabbing" : "grab",
                 transform: isDragging ? `rotate(${dragRotation}deg)` : undefined,
                 transition: "none",
